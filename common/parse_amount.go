de54e40dd179c099b7efff3c5a1fc10ec5f8f7f8
jiahong yap <yapjiahong@gmail.com>
2023-11-24 16:13:02 +0800

    feat: add parse_amount lib

--- common/parse_amount.go
+++ common/parse_amount.go
@@ -1,1 +1,25 @@
-package common
+package commona
+
+import "strings"
+
+func FormatAmount(amount string) string {
+	parts := strings.Split(amount, ".")
+	integerPart := parts[0]
+	decimalPart := parts[1]
+
+	// Add commas for thousands separator
+	integerPartWithCommas := addCommas(integerPart)
+
+	// Combine integer and decimal parts
+	formattedAmount := integerPartWithCommas + "." + decimalPart
+
+	return formattedAmount
+}
+
+func addCommas(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	return addCommas(s[:n-3]) + "," + s[n-3:]
+}
